6186b05a47dd8dee0d35af06285c0696fd4c64d5
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-04-25 10:21:59 -0500

    Image visibility toggled with button

--- src/components/imageList/Image.js
+++ src/components/imageList/Image.js
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap';
 
 const Image = (props) => {
     return ( 
-        <Card style={{ width: '18rem' }} className="mx-auto mb-2" >
+        <Card style={{ width: '40rem' }} className="mx-auto mb-2" >
   <Card.Img variant="top" src={props.pic} />
   <Card.Body>
     <Card.Title>Classified as: {props.name}</Card.Title>
--- src/components/imageList/ImageList.js
+++ src/components/imageList/ImageList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Image from './Image'
 import axios from 'axios';
+import { Button } from 'react-bootstrap';
 
 class ImageList extends Component {
   state = {
       images: [],
+      visible: 2,
   };
 
   componentDidMount() {
@@ -24,12 +26,19 @@ class ImageList extends Component {
       });
   };
 
+  handleVisible = () => {
+      const visible = this.state.visible
+      const new_visible = visible + 2
+      this.setState({visible:new_visible})
+  }
+
   render() {
-      const images = this.state.images.map(img=>{
+      const images = this.state.images.slice(0,this.state.visible).map(img=>{
           return <Image key={img.id} pic={img.picture} name={img.classfield} />
       })
     return <div><h1>Image List here</h1>
             {images}
+            <Button variant='primary' size='lg' onClick={this.handleVisible}>Load more</Button>
             </div>
   }
 }
