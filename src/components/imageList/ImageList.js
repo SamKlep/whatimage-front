19a7bf125ec52377718158b45dfe591f06f5c50d
Samuel Klepper <55067844+SamKlep@users.noreply.github.com>
2020-04-25 12:04:47 -0500

    Conditionals for displaying images and load more button

--- src/components/imageList/ImageList.js
+++ src/components/imageList/ImageList.js
@@ -8,7 +8,8 @@ class ImageList extends Component {
     images: [],
     visible: 2,
     isLoading: true,
-    newLoaded: false
+    newLoaded: false,
+    status: false,
   };
 
   componentDidMount() {
@@ -23,7 +24,10 @@ class ImageList extends Component {
         }
       })
       .then(resp => {
-        this.setState({ images: resp.data });
+        this.setState({ 
+            images: resp.data,
+            status: true,
+        });
         console.log(resp);
       });
     this.setState({ isLoading: false });
@@ -48,20 +52,26 @@ class ImageList extends Component {
     return (
       <div>
         <h1>Image List here</h1>
-        {this.state.isLoading ? (
+        {this.state.isLoading ? 
           <Spinner animation='border' role='status'></Spinner>
-        ) : (
+         : 
           <React.Fragment>
+            {((this.state.images.length === 0) && (this.state.status)) && 
+            <h3>No images classified</h3>
+            }
             {images}
-            {this.state.newLoaded && (
-              <Spinner animation='border' role='status'></Spinner>
-            )}
+            {this.state.newLoaded && 
+            <Spinner animation='border' role='status'></Spinner>
+            }
             <br />
-            <Button variant='primary' size='lg' onClick={this.handleVisible}>
-              Load more
-            </Button>
+            {((this.state.images.length > this.state.visible) && (this.state.images.length > 2)) && 
+            <Button variant='primary' size='lg' onClick={this.handleVisible}>Load more</Button>
+            }
+            {((this.state.images.length <= this.state.visible) && (this.state.images.length > 0)) && 
+            <h3>No more images to load</h3>
+            }
           </React.Fragment>
-        )}
+        }
       </div>
     );
   }
